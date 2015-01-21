aa9cd936ac46650b6ac35406f2fa70e07b975e4a
Oliver Switzer <oliver@littlebits.cc>
2015-01-21 10:40:18 -0500

    change event_properties.rb -> event_property.rb. Use uuid field for event_id in event_properties table

--- lib/generators/ahoy/stores/templates/active_record_event_properties_migration.rb
+++ lib/generators/ahoy/stores/templates/active_record_event_properties_migration.rb
@@ -3,7 +3,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
     create_table :ahoy_event_properties do |t|
 
       # event
-      t.integer :event_id
+      t.uuid :event_id
       t.string :name
       t.string :value
     end
