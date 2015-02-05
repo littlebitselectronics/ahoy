7b614bd77dc1a5988ad2a2f72029b4e824333090
Oliver Switzer <oliver@littlebits.cc>
2015-02-05 11:44:16 -0500

    remove before_save generate uuid filter on visitor model

--- lib/generators/ahoy/stores/templates/active_record_visitor_model.rb
+++ lib/generators/ahoy/stores/templates/active_record_visitor_model.rb
@@ -1,12 +1,4 @@
 class Visitor < ActiveRecord::Base
   has_many :events, class_name: "Ahoy::Event"
   has_many :visits, autosave: true
-
-  before_create :generate_uuid
-
-  private
-
-  def generate_uuid
-    self.id = SecureRandom.uuid
-  end
 end
